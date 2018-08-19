033e194bd07b8dfb05400f249a2d81db6344bb55
UncannyValet <psykloneranger@gmail.com>
2018-08-19 19:46:01 -0400

    [add] Partial models

--- vault/models.py
+++ vault/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+SYSTEM_CHOICES = (
+    ('4E', 'D&D 4th'),
+    ('5E', 'D&D 5th'),
+    ('BIND', 'Blades in the Dark'),
+    ('DW', 'Dungeon World'),
+)
+
+
+class Character(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default='')
+    system = models.CharField(max_length=16, choices=SYSTEM_CHOICES, default='5E')
+
+
+class Folder(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default='')
+    order = models.PositiveIntegerField
+
+
+class Item(models.Model):
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256, default='')
+    description = models.TextField(null='true')
